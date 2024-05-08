c6b0529b7d3ef9e9e8940fc4a99fcbc5744a7d96
Diogo Tavares <diogofilipe@ua.pt>
2024-05-08 17:43:54 +0100

    Atualizar tabela paletes

--- SQL/Create/create_tbl_paletes.sql
+++ SQL/Create/create_tbl_paletes.sql
@@ -2,5 +2,5 @@ CREATE TABLE paletes (
 	id_palete INT IDENTITY(1, 1) PRIMARY KEY,
 	peso DECIMAL(6, 2) NOT NULL,
 	temperatura DECIMAL(3, 1) NOT NULL,
-	id_pedido INT FOREIGN KEY REFERENCES pedidos(id_pedidos) NOT NULL
+	id_pedidos INT FOREIGN KEY REFERENCES pedidos(id_pedidos) NOT NULL
 );
