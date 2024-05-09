c9fd1e6203984a04e2e2ca46fde4c62328e0530d
Vicente Lourenço <134103080+VicLourenco40@users.noreply.github.com>
2024-05-09 15:19:52 +0100

    Adicionar tabela propósitos

---
+++ SQL/Tables/tbl_propositos.sql
@@ -0,0 +1,4 @@
+CREATE TABLE propositos (
+	id_proposito INT IDENTITY(1, 1) PRIMARY KEY,
+	descricao VARCHAR(32) NOT NULL
+);
--- SQL/Tables/tbl_rotas.sql
+++ SQL/Tables/tbl_rotas.sql
@@ -1,6 +1,6 @@
 CREATE TABLE rotas (
 	id_rota INT IDENTITY(1, 1) PRIMARY KEY,
-	proposito VARCHAR(32) NOT NULL,
+	id_proposito INT FOREIGN KEY REFERENCES propositos(id_proposito) NOT NULL,
 	id_planeador INT FOREIGN KEY REFERENCES trabalhadores(id_trabalhador) NOT NULL,
 	id_condutor INT FOREIGN KEY REFERENCES trabalhadores(id_trabalhador) NOT NULL,
 	id_camiao INT FOREIGN KEY REFERENCES camioes(id_camiao) NOT NULL
