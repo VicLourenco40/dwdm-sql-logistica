75df6593e38b6df00be418bde6dc50b8d1c41d73
Vicente Lourenço <134103080+VicLourenco40@users.noreply.github.com>
2024-05-09 17:39:13 +0100

    Corrigir dados

--- SQL/Dados/dados_rotas.sql
+++ SQL/Dados/dados_rotas.sql
@@ -1,20 +1,20 @@
-EXEC inserir_rota 1,1,5,1
-EXEC inserir_rota 1,1,5,6
-EXEC inserir_rota 1,4,8,11
-EXEC inserir_rota 2,4,11,7
-EXEC inserir_rota 2,7,14,12
-EXEC inserir_rota 1,7,17,14
-EXEC inserir_rota 2,10,20,3
-EXEC inserir_rota 1,10,23,8
-EXEC inserir_rota 2,13,26,13
-EXEC inserir_rota 2,13,5,15
-EXEC inserir_rota 2,16,5,5
-EXEC inserir_rota 2,16,8,10
-EXEC inserir_rota 1,19,11,2
-EXEC inserir_rota 1,22,14,15
-EXEC inserir_rota 1,22,17,4
-EXEC inserir_rota 1,25,20,9
-EXEC inserir_rota 1,25,23,1
-EXEC inserir_rota 2,25,26,11
-EXEC inserir_rota 2,25,29,7
-EXEC inserir_rota 1,13,29,2
+EXEC inserir_rota 1, 1, 5, 1
+EXEC inserir_rota 1, 1, 5, 6
+EXEC inserir_rota 1, 4, 8, 11
+EXEC inserir_rota 2, 4, 11, 7
+EXEC inserir_rota 2, 7, 14, 12
+EXEC inserir_rota 1, 7, 17, 14
+EXEC inserir_rota 2, 10, 20, 3
+EXEC inserir_rota 1, 10, 23, 8
+EXEC inserir_rota 2, 13, 26, 13
+EXEC inserir_rota 2, 13, 5, 15
+EXEC inserir_rota 2, 16, 5, 5
+EXEC inserir_rota 2, 16, 8, 10
+EXEC inserir_rota 1, 19, 11, 2
+EXEC inserir_rota 1, 22, 14, 15
+EXEC inserir_rota 1, 22, 17, 4
+EXEC inserir_rota 1, 25, 20, 9
+EXEC inserir_rota 1, 25, 23, 1
+EXEC inserir_rota 2, 25, 26, 11
+EXEC inserir_rota 2, 25, 29, 7
+EXEC inserir_rota 1, 13, 29, 2
