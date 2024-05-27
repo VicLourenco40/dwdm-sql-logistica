5c044a8ed520abaf0b2e297900b38e38fb63edc1
Vicente Lourenço <134103080+VicLourenco40@users.noreply.github.com>
2024-05-27 15:04:46 +0100

    Melhorar consulta 'recolha_urgente'

--- SQL/Consultas/recolha_urgente.sql
+++ SQL/Consultas/recolha_urgente.sql
@@ -1,28 +1,39 @@
--- A empresa "Nutrição Completa", que recebe um pedido no dia 2024-01-12,
--- necessita uma recolha urgente de 10 paletes nesse mesmo dia.
--- Verificar se o camião de entrega tem espaço suficiente para transportar estas
--- paletes de volta para o armazém.
+-- A empresa Nutrição Completa, que recebe um pedido no dia 2024-01-12,
+-- necessita uma recolha urgente de 10 paletes no mesmo dia.
+-- Verificar se o camião de entrega dessa rota tem espaço suficiente para
+-- transportar as paletes de volta para o armazém.
 
+DECLARE @id_destinatario INT;
+DECLARE @id_rota INT;
 DECLARE @max_paletes INT;
 DECLARE @num_paletes_rota INT;
 DECLARE @num_paletes_pedido INT;
+
+SET @id_destinatario = (
+	SELECT id_cliente
+	FROM clientes
+	WHERE nome LIKE 'Nutrição Completa'
+)
+
+SET @id_rota = (
+	SELECT r.id_rota
+	FROM rotas r
+	JOIN pedidos p ON p.id_rota = r.id_rota
+	WHERE p.id_destinatario = @id_destinatario
+	AND r.id_proposito = (
+		SELECT id_proposito
+		FROM propositos
+		WHERE descricao LIKE 'Entrega'
+	) AND p.data_entrega = '2024-01-12'
+)
 
 SET @max_paletes = (
 	SELECT max_paletes
 	FROM camioes
 	WHERE id_camiao = (
-		SELECT r.id_camiao
-		FROM rotas r
-		JOIN pedidos p ON p.id_rota = r.id_rota
-		WHERE p.id_destinatario = (
-			SELECT id_cliente
-			FROM clientes
-			WHERE nome LIKE 'Nutrição Completa'
-		) AND r.id_proposito = (
-			SELECT id_proposito
-			FROM propositos
-			WHERE descricao LIKE 'Entrega'
-		) AND p.data_entrega = '2024-01-12'
+		SELECT id_camiao
+		FROM rotas
+		WHERE id_rota = @id_rota
 	)
 )
 
@@ -32,20 +43,7 @@ SET @num_paletes_rota = (
 	WHERE id_pedido IN (
 		SELECT id_pedido
 		FROM pedidos
-		WHERE id_rota = (
-			SELECT r.id_rota
-			FROM rotas r
-			JOIN pedidos p ON p.id_rota = r.id_rota
-			WHERE p.id_destinatario = (
-				SELECT id_cliente
-				FROM clientes
-				WHERE nome LIKE 'Nutrição Completa'
-			) AND r.id_proposito = (
-				SELECT id_proposito
-				FROM propositos
-				WHERE descricao LIKE 'Entrega'
-			) AND p.data_entrega = '2024-01-12'
-		)
+		WHERE id_rota = @id_rota
 	)
 )
 
@@ -55,28 +53,12 @@ SET @num_paletes_pedido = (
 	WHERE id_pedido IN (
 		SELECT id_pedido
 		FROM pedidos
-		WHERE id_rota = (
-			SELECT r.id_rota
-			FROM rotas r
-			JOIN pedidos p ON p.id_rota = r.id_rota
-			WHERE p.id_destinatario = (
-				SELECT id_cliente
-				FROM clientes
-				WHERE nome LIKE 'Nutrição Completa'
-			) AND r.id_proposito = (
-				SELECT id_proposito
-				FROM propositos
-				WHERE descricao LIKE 'Entrega'
-			) AND p.data_entrega = '2024-01-12'
-		) AND id_destinatario = (
-			SELECT id_cliente
-			FROM clientes
-			WHERE nome LIKE 'Nutrição Completa'
-		)
+		WHERE id_rota = @id_rota
+		AND id_destinatario = @id_destinatario
 	)
 )
 
 IF @max_paletes - @num_paletes_rota + @num_paletes_pedido - 10 >= 0
 	PRINT 'O camião tem espaço suficiente para recolher as paletes'
 ELSE
-	PRINT 'Já foste Cândido'
+	PRINT 'O camião não tem espaço suficiente para recolher as paletes';
